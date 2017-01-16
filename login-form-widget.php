1ce06c6cf203de2cb5d0f7e6d06126fa94bba282
Kailey Lampert <trepmal@gmail.com>
2017-01-15 19:00:20 -0800

    general formatting

--- login-form-widget.php
+++ login-form-widget.php
@@ -18,55 +18,72 @@ function register_login_form_widget() {
 }
 class Login_Form_Widget extends WP_Widget {
 
+	/**
+	 *
+	 */
 	function __construct() {
-		$widget_ops = array('classname' => 'login-form-widget', 'description' => __( 'Show a login form.', 'login-form-widget' ) );
-		$control_ops = array( 'width' => 300 );
-		parent::WP_Widget( 'login_form_widget', __( 'Login Form', 'login-form-widget' ), $widget_ops, $control_ops );
+		$widget_ops = array(
+			'classname'   => 'login-form-widget',
+			'description' => __( 'Show a login form.', 'login-form-widget' )
+		);
+		$control_ops = array();
+		parent::__construct( 'login_form_widget', __( 'Login Form', 'login-form-widget' ), $widget_ops, $control_ops );
 	}
 
+	/**
+	 *
+	 */
 	function widget( $args, $instance ) {
 
-		extract( $args, EXTR_SKIP );
-		echo $before_widget;
+		echo $args['before_widget'];
 
-		echo $instance['hide_title'] ? '' : $before_title . apply_filters( 'widget_title', $instance['title'] ) . $after_title;
+		echo $instance['hide_title'] ? '' : $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 
 		if ( is_user_logged_in() ) {
 			echo '<ul><li>';
 			wp_loginout();
 			echo '</li>';
-			wp_register();
+			wp_register(); // defaults to being wrapped in <li></li>
 			echo '</ul>';
 		} else {
 			wp_login_form();
 		}
 
-		echo $after_widget;
+		echo $args['after_widget'];
 
 	} //end widget()
 
-	function update($new_instance, $old_instance) {
+	/**
+	 *
+	 */
+	function update( $new_instance, $old_instance ) {
 
 		$instance = $old_instance;
-		$instance['title'] = esc_attr( $new_instance['title'] );
+
+		$instance['title']      = esc_attr( $new_instance['title'] );
 		$instance['hide_title'] = (bool) $new_instance['hide_title'] ? 1 : 0;
 		return $instance;
 
 	} //end update()
 
+	/**
+	 *
+	 */
 	function form( $instance ) {
-
-		$instance = wp_parse_args( (array) $instance, array( 'title' => 'Login', 'hide_title' => 0 ) );
-		extract( $instance );
+		$defaults = array(
+			'title'      => 'Log in',
+			'hide_title' => 0,
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults );
 		?>
-		<p style="width:63%;float:left;">
+		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'login-form-widget' );?>
-				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
 			</label>
-		</p>
-		<p style="width:33%;float:right;padding-top:20px;height:20px;">
-			<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('hide_title'); ?>" name="<?php echo $this->get_field_name('hide_title'); ?>"<?php checked( $hide_title ); ?> />
-			<label for="<?php echo $this->get_field_id('hide_title'); ?>"><?php _e( 'Hide Title?', 'login-form-widget' );?></label>
+			<span>
+				<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('hide_title'); ?>" name="<?php echo $this->get_field_name('hide_title'); ?>"<?php checked( $instance['hide_title'] ); ?> />
+				<label for="<?php echo $this->get_field_id('hide_title'); ?>"><?php _e( 'Hide Title?', 'login-form-widget' );?></label>
+			</span>
 		</p>
 		<?php
 
